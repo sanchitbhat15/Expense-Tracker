aedb590ac1aefb28ada320d59391325d32566649
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-07-05 00:48:45 +0530

    premuim.js added

---
+++ sendFile/frontend/premium.js
@@ -0,0 +1,11 @@
+function getinfo(e){
+    e.preventDefault()
+
+    axios.get('http://localhost:3000/getinfo/getinfopremium').then(res=>{
+        console.log(res)
+       
+        document.body.innerHTML+=`<div>${res.data[0].expenseamount}</div>`
+    }).catch(err=>{
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
+}
