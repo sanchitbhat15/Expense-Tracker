e5f6b7f9020ea35afc933915f120b6c2b092a07d
sanchitbhat15 <bhat.sanchit@gmail.com>
2021-07-05 00:46:48 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const path = require('path');
 
 const express = require('express');
 var cors = require('cors')
+const authorization=require('./middleware/auth')
 const sequelize = require('./util/database');
 const User = require('./models/users');
 const Expense = require('./models/expenses');
+const Password = require('./models/password')
+const Order = require('./models/pay')
 
 const userRoutes = require('./routes/user')
+const ExpenseReport = require('./routes/auth')
+const PaymentRoute = require('./routes/payment')
+const PasswordRoute = require('./routes/resetpwd')
+
 
 const app = express();
 const dotenv = require('dotenv');
@@ -20,10 +27,21 @@ app.use(cors());
 // app.use(bodyParser.urlencoded());  ////this is for handling forms
 app.use(express.json());  //this is for handling jsons
 
-app.use('/user', userRoutes)
+app.use('/expenseReport',authorization,expensereport);
 
-User.hasMany(Expense);
-Expense.belongsTo(User);
+app.use('/user', userRoutes)
+app.use('/payment',paymentRoutes)
+app.use('/getinfo',premiumfeature)
+app.use('/password',passwordRoutes)
+
+
+Users.hasMany(Expense)
+Expense.belongsTo(Users)
+Users.hasMany(Order)
+Order.belongsTo(Users)
+
+Users.hasMany(Password)
+Password.belongsTo(Users)
 
 
 sequelize.sync()
